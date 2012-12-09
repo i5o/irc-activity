36b9990c7547a0bcd96b9176f16af07c6569c1bb
Aneesh Dogra <lionaneesh@gmail.com>
2012-12-09 18:04:13 +0530

    Some more fixes.

--- purk/windows.py
+++ purk/windows.py
@@ -160,13 +160,13 @@ class SimpleWindow(Window):
         self.focus = self.input.grab_focus
         self.connect("key-press-event", self.transfer_text)
 
-        self.pack_end(self.input, False, True, 0)
+        self.pack_end(self.input, True, False, 0)
 
         topbox = Gtk.ScrolledWindow()
         topbox.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
         topbox.add(self.output)
 
-        self.pack_end(topbox, False, True, 0)
+        self.pack_end(topbox, True, False, 0)
 
         self.show_all()
 
